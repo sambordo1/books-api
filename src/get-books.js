2bf76a081da724c9b003636ad08cac8b20f3dbb9
Sam Bordo <sambordo1@gmail.com>
2022-02-18 14:52:52 -0500

    added get-book lambda function

--- src/get-books.js
+++ src/get-books.js
@@ -6,7 +6,6 @@ exports.handler = async event => {
     TableName: "books" // The name of your DynamoDB table
   };
   try {
-    // Utilising the scan method to get all items in the table
     const data = await documentClient.scan(params).promise();
     const response = {
       statusCode: 200,
